648506d3a291301cd9619e840fe1e71e0d6cf7f7
Jagdish Mevada <jagmevada@gmail.com>
2024-06-29 16:04:52 +0530

    Using RTC switching between active/sleep mode and generate PWM on PB0,1,2 in active mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,27 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/sleep.h>
-// ATTINY3224/6 has 3 timers  (TCA, TCB0 & TCB1), TCB1 is used by millis, so
-// only TCB0 & TCA available TCA has three Compare and 1 period, so 3 PWM and 1
-// Overflow ISR and 3 compare ISR can be generated this code demonstrate TCA0
-// CMP0 ISR, and TCB0 CMP ISR. this code also demonstrate RTC 1KHz sleep mode
-// running in sleep mode as ULPM, Sleeps for 5seconds then activate CPU for 5sec
-// in which TCA or TCB0 will toggle LED on PA7(3) pin at 1Hz.
-// Also demonstrate wakeup from sleep with PC0(10) button input and Toggle LED
-// on PA7(3).
+// This code also demonstrate RTC 1KHz sleep mode running in sleep mode as ULPM,
+// Sleeps for 5seconds then activate CPU for 5sec in which TCA generates 3 PWM
+// on PB0,1,2 with different duty cycle and TCB0 will toggle
+// LED on PA7(3) pin at 1Hz. Also demonstrate wakeup from sleep with PC0(10)
+// button input and Toggle LED on PA7(3).
+// Pl. note PB0-2 set as input pullup when going sleep mode and set as output in
+// active mode, this is done in RTC ISR.
 uint8_t state = 0;
 void setup_timer_a() {
   takeOverTCA0();
-  TCA0_SINGLE_PER = 10000;  // PER/F_CPU, 10ms at 1MHz
-  TCA0_SINGLE_CMP0 = 2500;  // intterupt at CMP0<TOP, TOP=PER;
+  TCA0_SINGLE_PER = 1000;  // PER/F_CPU, 1ms at 1MHz
+  TCA0_SINGLE_CMP0 = 250;  // intterupt at CMP0<TOP, TOP=PER;
+  TCA0_SINGLE_CMP1 = 750;
+  TCA0_SINGLE_CMP2 = 500;
   TCA0_SINGLE_CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc |  // F_CPU * DIV1
                       TCA_SINGLE_ENABLE_bm;        // TCA_SINGLE_RUNSTDBY_bm;
-  TCA0_SINGLE_INTFLAGS =
-      TCA_SINGLE_CMP0_bm;  // Clear Any pending interrupt flags
-  TCA0_SINGLE_INTCTRL = TCA_SINGLE_CMP0_bm;  // Enable TCA0 timeout interrupt
+  TCA0_SINGLE_CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc | TCA_SINGLE_CMP2EN_bm |
+                      TCA_SINGLE_CMP1EN_bm | TCA_SINGLE_CMP0EN_bm;
+  // TCA0_SINGLE_INTFLAGS =
+  //     TCA_SINGLE_CMP0_bm;  // Clear Any pending interrupt flags
+  // TCA0_SINGLE_INTCTRL = TCA_SINGLE_CMP0_bm;  // Enable TCA0 timeout interrupt
 };
 
 void setup_timer_b0() {
@@ -53,10 +56,16 @@ void setup() {
   pinMode(5, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
+  // pinMode(7, INPUT_PULLUP);
+  // pinMode(8, INPUT_PULLUP);
+  // pinMode(9, INPUT_PULLUP);
   pinMode(10, INPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  digitalWrite(7, 0);
+  digitalWrite(8, 0);
+  digitalWrite(9, 0);
   pinMode(11, INPUT_PULLUP);
   pinMode(12, INPUT_PULLUP);
   pinMode(13, INPUT_PULLUP);
@@ -70,7 +79,7 @@ void setup() {
   sleep_enable();
   attachInterrupt(10, button5, CHANGE);
   setup_timer_a();
-  // setup_timer_b0(); // use either a0 or b0
+  setup_timer_b0();  // use either a0 or b0
   setup_timer_rtc();
   sei();  // Enable global interrupts
 }
@@ -120,8 +129,20 @@ ISR(RTC_PIT_vect) {  /// 1sec timer  /// 5 sleep and 5 sec awake
     timercount = sec;
     xx = !xx;
     if (xx) {
+      pinMode(7, OUTPUT);
+      pinMode(8, OUTPUT);
+      pinMode(9, OUTPUT);
+
       sleep_disable();
     } else {
+      // PORTB_OUT &= ~(PIN0_bm | PIN1_bm | PIN2_bm);
+      // digitalWrite(7, 1);
+      // digitalWrite(8, 1);
+      // digitalWrite(9, 1);
+      pinMode(7, INPUT_PULLUP);  // Only pullup saves power in sleep mode ,
+                                 // output low doesnt
+      pinMode(8, INPUT_PULLUP);
+      pinMode(9, INPUT_PULLUP);
       sleep_enable();
     }
     // digitalWrite(3, xx);
