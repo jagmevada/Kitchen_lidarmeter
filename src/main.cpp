bc48003f8aa06d4e6c982ddf71c3c86b1a7a070f
Jagdish Mevada <jagmevada@gmail.com>
2024-06-29 12:14:24 +0530

    Sleep mode RTC, wake up from RTC or button input and run TCA, TCB0 timers to toggle LED

--- src/main.cpp
+++ src/main.cpp
@@ -2,53 +2,29 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/sleep.h>
-
-// void initialize() {
-//   init_clock();
-//   // init_ADC0();
-//   init_timers();
-//   init_millis();
-//   // init();
-//   //  RTC_init(); /* Initialize the RTC timer */
-//   pinMode(0, INPUT_PULLUP);
-//   pinMode(1, INPUT_PULLUP);
-//   pinMode(2, INPUT_PULLUP);
-//   pinMode(3, INPUT_PULLUP);
-//   pinMode(4, INPUT_PULLUP);
-//   pinMode(5, INPUT_PULLUP);
-//   pinMode(6, INPUT_PULLUP);
-//   pinMode(7, INPUT_PULLUP);
-//   pinMode(8, INPUT_PULLUP);
-//   pinMode(9, INPUT_PULLUP);
-//   pinMode(10, INPUT);
-//   pinMode(11, INPUT_PULLUP);
-//   pinMode(12, INPUT_PULLUP);
-//   pinMode(13, INPUT_PULLUP);
-//   pinMode(14, INPUT_PULLUP);
-//   pinMode(15, INPUT_PULLUP);
-//   pinMode(16, INPUT_PULLUP);
-//   set_sleep_mode(SLEEP_MODE_PWR_DOWN); /* Set sleep mode to POWER DOWN mode
-//   */ sleep_enable(); /* Enable sleep mode, but not going to sleep yet */
-// }
-
-// int main() {
-//   initialize();
-//   while (1) {
-//     sleep_cpu(); /* Sleep the device and wait for an interrupt to continue */
-//     // digitalWrite(PIN_PA6, CHANGE); /* Device woke up and toggle LED on
-//     pin#7
-//     // */
-//   }
-//   return (0);
-// }
-
-// #include <Arduino.h>
-// #include <avr/sleep.h>
+// ATTINY3224/6 has 3 timers  (TCA, TCB0 & TCB1), TCB1 is used by millis, so
+// only TCB0 & TCA available TCA has three Compare and 1 period, so 3 PWM and 1
+// Overflow ISR and 3 compare ISR can be generated this code demonstrate TCA0
+// CMP0 ISR, and TCB0 CMP ISR. this code also demonstrate RTC 1KHz sleep mode
+// running in sleep mode as ULPM, Sleeps for 5seconds then activate CPU for 5sec
+// in which TCA or TCB0 will toggle LED on PA7(3) pin at 1Hz.
+// Also demonstrate wakeup from sleep with PC0(10) button input and Toggle LED
+// on PA7(3).
 uint8_t state = 0;
+void setup_timer_a() {
+  takeOverTCA0();
+  TCA0_SINGLE_PER = 10000;  // PER/F_CPU, 10ms at 1MHz
+  TCA0_SINGLE_CMP0 = 2500;  // intterupt at CMP0<TOP, TOP=PER;
+  TCA0_SINGLE_CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc |  // F_CPU * DIV1
+                      TCA_SINGLE_ENABLE_bm;        // TCA_SINGLE_RUNSTDBY_bm;
+  TCA0_SINGLE_INTFLAGS =
+      TCA_SINGLE_CMP0_bm;  // Clear Any pending interrupt flags
+  TCA0_SINGLE_INTCTRL = TCA_SINGLE_CMP0_bm;  // Enable TCA0 timeout interrupt
+};
 
 void setup_timer_b0() {
-  TCB0.CCMP = 5000;                                    // CCMP/1MHz.
-  TCB0.CTRLA = TCB_CLKSEL_CLKDIV2_gc | TCB_ENABLE_bm;  //| TCB_RUNSTDBY_bm;
+  TCB0.CCMP = 10000;                                   // CCMP/1MHz, 10ms
+  TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm;  //| TCB_RUNSTDBY_bm;
   TCB0.INTFLAGS = TCB_CAPT_bm;  // Clear any pending interrupt flags
   TCB0.INTCTRL = TCB_CAPT_bm;   // Enable TCB0 timeout interrupt
 };
@@ -63,19 +39,14 @@ void setup_timer_rtc() {
 }
 void button5() {
   if (digitalRead(10) == HIGH) {
-    // state = !state;
-    // digitalWrite(3, state);
+    state = !state;
+    digitalWrite(3, state);
   }
 }
 
 void setup() {
   pinMode(3, OUTPUT);
   digitalWrite(3, LOW);
-  // _PROTECTED_WRITE(CLKCTRL_MCLKCTRLA, CLKCTRL_CLKSEL1_bm);
-  // _PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, CLKCTRL_PEN_bp);
-  // _PROTECTED_WRITE(CLKCTRL.OSC32KCTRLA, CLKCTRL_RUNSTDBY_bm);
-  // while (!(CLKCTRL_MCLKSTATUS & CLKCTRL_OSC32KS_bm));
-  // _PROTECTED_WRITE()
   pinMode(0, INPUT_PULLUP);
   pinMode(1, INPUT_PULLUP);
   pinMode(2, INPUT_PULLUP);
@@ -98,12 +69,10 @@ void setup() {
   set_sleep_mode(SLEEP_MODE_STANDBY);
   sleep_enable();
   attachInterrupt(10, button5, CHANGE);
-
-  setup_timer_b0();
+  setup_timer_a();
+  // setup_timer_b0(); // use either a0 or b0
   setup_timer_rtc();
   sei();  // Enable global interrupts
-  // set_sleep_mode(SLEEP_MODE_STANDBY);
-  // sleep_mode();
 }
 
 void loop() {
@@ -116,11 +85,25 @@ void loop() {
   // digitalWrite(3, 0);
 }
 
-ISR(TCB0_INT_vect) {  /// 10ms timer  // active only when awake
-  static unsigned int timercount = 50, xx = 0;
+ISR(TCA0_CMP0_vect) {  /// 10ms timer  // active only when awake
+  static unsigned int t0 = 50, timercount = t0, xx = 0;
   timercount--;
   if (timercount == 0) {
-    timercount = 50;
+    timercount = t0;
+    xx = !xx;
+    digitalWrite(3, xx);
+    // PORTA_OUT = xx << 7;
+    // PORTA_OUTTGL |= PIN7_bm;
+  }
+  TCA0_SINGLE_INTFLAGS = TCA_SINGLE_CMP0_bm; /* Clear the interrupt flag */
+  // sleep_cpu();
+}
+
+ISR(TCB0_INT_vect) {  ///  // active only when awake
+  static unsigned int t0 = 50, timercount = t0, xx = 0;
+  timercount--;
+  if (timercount == 0) {
+    timercount = t0;
     xx = !xx;
     // digitalWrite(3, xx);
     PORTA_OUT = xx << 7;
