f05595f7577e8eacc75166c363f0710d8f6a2e57
Jagdish Mevada <jagmevada@gmail.com>
2024-08-03 14:48:40 +0530

    LPM tested with 80uA ADC battery monitoring added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
-// #include <avr/iotn3226.h>
 #include <avr/sleep.h>
 #include <function.h>
 typedef ADC_MUXPOS_t adc_0_channel_t;
@@ -29,38 +28,66 @@ typedef ADC_MUXPOS_t adc_0_channel_t;
 #define OUTPUT_PINMASK PIN5_bm
 #define DEBOUNCE_PERIOD 5     /// Multiple of 128ms
 #define AUTOOFF_TIMEOUT 2445  // 2445  // 3130  Multiple of 128ms or 100ms
-#define ADC_PERIOD 156        // 20s at multiple of 128ms
+#define ADC_PERIOD 156        // 6        // 20s at multiple of 128ms
 #define BAT_LOW_TIMEOUT 7
-#define UTH 3200
+#define UTH 3300
 #define LTH 2800
 #define LOW 0
 #define HIGH 1
-/*
-    Main application
-*/
+
 // ######################### variables ########################################
 bool new_input, batstate = 1, ldostate = 0, ldostateprev = 0;
 unsigned int vbat = 4095, vbatx = 4095;
 unsigned char t0 = DEBOUNCE_PERIOD, t2 = 0;
 uint16_t t1 = ADC_PERIOD;
 uint16_t timeout = 0;
-
+boolean txready = 0;
 // ######################### function defs ####################################
+void init_variable();
 void Timer_A0();
 void button_input();
-void button5();
 void setup_timer_a();
 void setup_timer_b0();
 void setup_timer_rtc();
 void ADC0_StartConversion(adc_0_channel_t channel);
 void ADC0_Enable(void);
 void ADC0_Disable(void);
-int8_t ADC0_Initialize(void);
+void ADC0_Initialize(void);
+void presleep();
+
+// // ######################### presleep() #########################
+void presleep() {
+  PORTA.OUT &= ~OUTPUT_PINMASK;  // turn off LDO bat low
+  pinMode(0, OUTPUT);            // ADC
+  digitalWrite(0, LOW);
+  ldostateprev = 1;
+  ldostate = 0;
+  ADC0_Disable();
+  Serial1.end();
+  pinMode(8, OUTPUT);
+  digitalWrite(8, 0);
+  pinMode(9, INPUT_PULLUP);
+}
+
+// // ######################### init variables #########################
+void init_variable() {
+  new_input = 0;
+  batstate = 1;
+  ldostate = 0;
+  ldostateprev = 0;
+  vbat = 4095;
+  vbatx = 4095;
+  t0 = DEBOUNCE_PERIOD;
+  t2 = 0;
+  t1 = 20;
+  timeout = 0;
+  txready = 0;
+}
 
 // // ######################### adc_startconv function #########################
 void ADC0_StartConversion(adc_0_channel_t channel) {
   ADC0.MUXPOS &= ADC_VIA_gm;
-  ADC0.MUXPOS |= channel;
+  ADC0.MUXPOS |= channel;  // VDDDIV10
   ADC0.COMMAND &= ~ADC_DIFF_bm;
   ADC0.COMMAND |= ADC_START_IMMEDIATE_gc;
 }
@@ -72,72 +99,41 @@ void ADC0_Disable(void) { ADC0.CTRLA &= ~ADC_ENABLE_bm; }
 
 // ####################### adc_initializaion function  ######################
 
-int8_t ADC0_Initialize(void) {
+void ADC0_Initialize(void) {
   // PRESC System clock divided by 64;
   ADC0.CTRLB = 0xF;
-
   // FREERUN disabled; LEFTADJ disabled; SAMPNUM No accumulation;
   ADC0.CTRLF = 0x0;
-
   // REFSEL Internal 2.048V Reference; TIMEBASE 1;
   ADC0.CTRLC = 0xD;
-
   // WINCM No Window Comparison; WINSRC RESULT;
   ADC0.CTRLD = 0x0;
-
   // SAMPDUR 255;
-  ADC0.CTRLE = 0xFF;
-
+  ADC0.CTRLE = 0xFF;  // CLK_ADC
   // ADCPGASAMPDUR 6 ADC cycles; GAIN 1X Gain; PGABIASSEL 1x BIAS current; PGAEN
   // disabled;
   ADC0.PGACTRL = 0x0;
-
   // DBGRUN disabled;
   ADC0.DBGCTRL = 0x0;
-
   // DIFF disabled; MODE SINGLE_12BIT; START Stop an ongoing conversion;
   ADC0.COMMAND = 0x10;
-
   // RESOVR disabled; RESRDY enabled; SAMPOVR disabled; SAMPRDY disabled;
   // TRIGOVR disabled; WCMP disabled;
   ADC0.INTCTRL = 0x1;
-
   // MUXPOS ADC input pin 4; VIA Via ADC;
   ADC0.MUXPOS = 0x4;
-
   // MUXNEG Ground; VIA Via ADC;
   ADC0.MUXNEG = 0x30;
-
   // Window comparator high threshold
   ADC0.WINHT = 0x0;
-
   // Window comparator low threshold
   ADC0.WINLT = 0x0;
-
   // ENABLE disabled; LOWLAT disabled; RUNSTDBY disabled;
   ADC0.CTRLA = 0x0;
-
-  return 0;
 }
 
 // ######################### timer_a0 isr function #############################
 void Timer_A0(void) {  // 100ms
-
-  static unsigned char tx = 5;
-  if (tx == 0) tx = 5;
-  tx--;
-
-  if (tx == 1) {  /// output blink before permanently low
-    if (t2 >
-        0) {  // bat low timeout count to zero to blink and then LDO set to LOW
-      t2--;
-      if (t2 == 0) {
-        PORTA.OUT &= ~OUTPUT_PINMASK;  // turn off LDO
-      }
-    }
-  }
-
-  //        SLPCTRL.CTRLA = 0x3;
 }
 // ######################### button_input #############################
 void button_input(void) {
@@ -150,27 +146,20 @@ void button_input(void) {
   if (ldostate) {                 // LDO to be turned on
     PORTA.OUT |= OUTPUT_PINMASK;  // turn on again but
     ADC0_Enable();
+    t1 = 20;
     timeout = AUTOOFF_TIMEOUT;
-    pinMode(0, INPUT);             // ADC
-  } else {                         // LDO to be turned off
-    PORTA.OUT &= ~OUTPUT_PINMASK;  // turn off LDO bat low
-    ADC0_Disable();
-    timeout = 0;
-    pinMode(0, OUTPUT);  // ADC
-    digitalWrite(0, LOW);
+    pinMode(0, INPUT);  // ADC
+    pinMode(8, INPUT_PULLUP);
+    pinMode(9, INPUT_PULLUP);
+    Serial1.begin(9600);
+  } else {  // LDO to be turned off
+    presleep();
   }
   //}
   PORTA.INTFLAGS = 0x40;  // PA6 // clear INTFLAG
   //    SLPCTRL.CTRLA = 0x3; // enable sleep and go to standby mode
 }
-// ######################### button5 #############################
-uint8_t state = 0;
-void button5() {
-  if (digitalRead(10) == HIGH) {
-    state = !state;
-    digitalWrite(3, state);
-  }
-}
+
 // ######################### setup_timer_a0 #############################
 void setup_timer_a() {
   takeOverTCA0();
@@ -200,41 +189,29 @@ void setup_timer_rtc() {
 }
 //@@@@@@@@@@@@@@@@@@@@@@@@@  SETUP  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 void setup() {
+  init_variable();
   pinMode(0, OUTPUT);  // ADC
   digitalWrite(0, LOW);
   SETUPLDO();
   LDODISABLE();
   pinMode(2, INPUT);  // SW IN external pulled up
   pinMode(3, OUTPUT);
-  digitalWrite(3, LOW);
-  // it is external pulled down pinMode(5, INPUT_PULLUP);
+  digitalWrite(3, LOW);  // it is external pulled down pinMode
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
   pinMode(6, INPUT_PULLUP);
   pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);  // TXPIN
   pinMode(9, INPUT_PULLUP);  // CHARGE IN OPTIONAL, NEED EX. PULLDOWN AND IT
                              // WILL RISE UP WHEN USB CONNECTED
   pinMode(10, INPUT_PULLUP);
-  // pinMode(11, INPUT_PULLUP);
-  // pinMode(12, INPUT_PULLUP);
-  // pinMode(13, INPUT_PULLUP);
-  // pinMode(14, INPUT_PULLUP);
-  // pinMode(15, INPUT_PULLUP);
-  // pinMode(16, INPUT_PULLUP);
-  // ADC0.CTRLA &= ~ADC_ENABLE_bm;
-  //   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  // set_sleep_mode(SLEEP_MODE_IDLE); //380UA
   batstate = HIGH;
   new_input = 0;
-  // ADC0_Disable();
-  set_sleep_mode(SLEEP_MODE_STANDBY);
-
-  attachInterrupt(SWINPUT, button_input, FALLING);
-  // setup_timer_a();
+  presleep();
   ADC0_Initialize();
   ADC0_Disable();
-  // setup_timer_b0(); // use either a0 or b0
+  set_sleep_mode(SLEEP_MODE_STANDBY);
+  attachInterrupt(SWINPUT, button_input, FALLING);
   setup_timer_rtc();
   sleep_enable();
   sei();  // Enable global interrupts
@@ -244,76 +221,70 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // ADC0.CTRLA &= ~ADC_ENABLE_bm;
-  sleep_cpu();
+  if (txready) {
+    Serial1.write((uint8_t)(vbatx / 20));
+    txready = 0;
+  }
+  if (ldostate == 0) {
+    sleep_cpu();
+  }
 }
 
 // ************************* ISR Timer RTC ***************************
 ISR(RTC_PIT_vect) {  /// 128 msec timer
-
-  // static unsigned int sec = 5, timercount = sec, xx = 0;
-  // timercount--;
-  // if (timercount == 0) {
-  //   timercount = sec;
-  //   xx = !xx;
-  //   if (xx) {
-  //     sleep_disable();
-  //   } else {
-  //     sleep_enable();
-  //   }
-  //   // digitalWrite(3, xx);
-  // }
-  if (new_input) {
-    t0--;           // debounce timer countdown
-    if (t0 == 0) {  // button debounce period over
+  if (new_input) {   /// routine for enable button interrupt after debounce
+                     /// period
+    t0--;            // debounce timer countdown
+    if (t0 == 0) {   // button debounce period over
       new_input = 0;
       SW_INTCTRL = INTERRUPT_EN;  // enable interrupt
       PORTA.INTFLAGS = PIN6_bm;   // clear previous flags
-      t1 = ADC_PERIOD;
     }
   }
 
-  if (timeout > 0) {  // Normal autoff timeout countdown
+  if (timeout > 0) {  // Normal autoff timeout countdown, set to AUTOOFF_TIMEOUT
+                      // when power button pressed
     timeout--;
-    if (timeout == 0) {              // Normal autoff timeout event
-      PORTA.OUT &= ~OUTPUT_PINMASK;  // turn off LDO bat low
-      ldostateprev = 0;
-      ldostate = 0;
-      ADC0_Disable();
+    if (timeout == 0) {  // Normal autoff timeout event
+      presleep();
     }
   }
+
   if (ldostate == 1) {  // Vbat monitoring and auto cutoff
-    if (t1 == 0) {      // adc timing
+
+    if (t1 == 0) {  // adc timing
       t1 = ADC_PERIOD;
       vbatx = vbat;  // Copy previous conversoin result
       ADC0_StartConversion(ADC_MUXPOS_AIN4_gc);
+      txready = 1;
     }
     t1--;
 
-    //    static unsigned char batstate=LOW;
-    if (batstate == HIGH) {  // discharging to cutoff
+    if (batstate == HIGH) {  // battery is discharging to cutoff
       if (vbatx < LTH) {     // reached at cutoff
         batstate = LOW;
-        timeout = 1;
-        // if ((PORTA.OUT & OUTPUT_PINMASK) == OUTPUT_PINMASK) {  /// if LDO
-        // ON??
+        timeout = 0;
+        presleep();
       }
     } else {  // charging to reach full charge voltage 4.2V
       if (vbatx > UTH) {
         batstate = HIGH;
       }
     }
+  } else {
+    sleep_enable();
   }
-  sleep_enable();
   RTC.PITINTFLAGS = RTC_PI_bm; /* Clear the interrupt flag */
 }
+
 // ************************* ISR ADC Result ready  ***************************
 ISR(ADC0_RESRDY_vect) {
   /* Insert your ADC result ready interrupt handling code here */
-  vbat = (uint8_t)ADC0.RESULT;
+  vbat = (uint16_t)ADC0.RESULT;
+
   /* The interrupt flag has to be cleared manually */
   ADC0.INTFLAGS = ADC_RESRDY_bm;
 }
-
 // ************************* ISR Timer A0 ***************************
 ISR(TCA0_CMP0_vect) {  /// 10ms timer  // active only when awake
   TCA0_SINGLE_INTFLAGS = TCA_SINGLE_CMP0_bm; /* Clear the interrupt flag */
